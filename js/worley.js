27620cc209825a35dc5633d7a726d350d8b12f34
egillmagnus <124277422+egillmagnus@users.noreply.github.com>
2024-03-05 20:13:23 +0000

    Laga menu virkni á mobile worley

--- js/worley.js
+++ js/worley.js
@@ -80,3 +80,11 @@ function draw() {
         }
     }
 }
+
+let menuicon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar")
+
+menuicon.onclick = () => {
+    menuicon.classList.toggle("bx-x");
+    navbar.classList.toggle("active");
+}
