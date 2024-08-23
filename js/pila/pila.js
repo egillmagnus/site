de0f8881cd0fae84d23349ee563fd894a56dceee
BuildTools <unconfigured@null.spigotmc.org>
2024-08-23 18:14:32 +0000

    Bætti við sierpinskis gasket

--- js/pila/pila.js
+++ js/pila/pila.js
@@ -182,3 +182,12 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+
+let menuicon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar")
+
+menuicon.onclick = () => {
+    menuicon.classList.toggle("bx-x");
+    navbar.classList.toggle("active");
+}
