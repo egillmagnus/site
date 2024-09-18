b38606c6977cd68a3513b808cc35e2071bb4ee6c
egillmagnus <to.egill@gmail.com>
2024-09-18 14:56:34 +0000

    Smá breytingar á draw circle

--- js/tolvugrafik/hd3/drawCircle.js
+++ js/tolvugrafik/hd3/drawCircle.js
@@ -1,8 +1,7 @@
 var gl;
 
-window.onload = function init()
-{
-    var canvas = document.getElementById( "gl-canvas" );
+window.onload = function init() {
+    var canvas = document.getElementById("gl-canvas");
 
     let menuicon = document.querySelector("#menu-icon");
     let navbar = document.querySelector(".navbar");
@@ -13,48 +12,48 @@ window.onload = function init()
     };
 
     setCanvasSize(canvas);
-    
-    gl = WebGLUtils.setupWebGL( canvas );
-    if ( !gl ) { alert( "WebGL isn't available" ); }
 
-    gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
-    
-    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram( program );
- 
-    var vertices = [ vec2( -1, -1 ), vec2( 1,  -1 ), vec2( 1, 1 ),
-                     vec2( -1, -1 ), vec2( 1,  1 ), vec2( -1, 1 )
+    gl = WebGLUtils.setupWebGL(canvas);
+    if (!gl) { alert("WebGL isn't available"); }
+
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+
+    var program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
+
+    var vertices = [vec2(-1, -1), vec2(1, -1), vec2(1, 1),
+    vec2(-1, -1), vec2(1, 1), vec2(-1, 1)
     ];
 
     var bufferId = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
 
-    var vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition );
+    var vPosition = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
 
-    locTime = gl.getUniformLocation( program, "time" );
-    
+    locTime = gl.getUniformLocation(program, "time");
+
     iniTime = Date.now();
 
     canvasRes = vec2(canvas.width, canvas.height);
-    gl.uniform2fv( gl.getUniformLocation( program, "resolution" ), flatten(canvasRes) )
+    gl.uniform2fv(gl.getUniformLocation(program, "resolution"), flatten(canvasRes))
 
     render();
 };
 
 
 function render() {
-    gl.clear( gl.COLOR_BUFFER_BIT );
+    gl.clear(gl.COLOR_BUFFER_BIT);
 
     var msek = Date.now() - iniTime;
 
-    var time = ((msek*0.0005) % 2) - 1;
-    gl.uniform1f( locTime, time );
-    
-    gl.drawArrays( gl.TRIANGLES, 0, 6 );
+    var time = ((msek * 0.0005));
+    gl.uniform1f(locTime, time);
+
+    gl.drawArrays(gl.TRIANGLES, 0, 6);
 
     window.requestAnimFrame(render);
 }
